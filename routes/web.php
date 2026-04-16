dc506d9fb2bb024d59327ea4b1fbbfecd0633d09
Kayhan Aziz <kayhan.aziz.nebi@gmail.com>
2026-04-16 16:00:13 +0300

    fix: Update GoogleController to use stateless method and change session driver to file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\Auth\GoogleController;
 
 Route::get('/', function () {
     return 'OK';
@@ -8,6 +9,10 @@ Route::get('/', function () {
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
+});
+Route::middleware(['web'])->group(function () {
+    Route::get('/auth/google', [GoogleController::class, 'redirect']);
+    Route::get('/auth/google/callback', [GoogleController::class, 'callback']);
 });
 
 require __DIR__.'/auth.php';
