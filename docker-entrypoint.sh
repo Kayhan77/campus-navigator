2f7ab22eabc09af8d093d89363464dbf6613275e
Kayhan Aziz <kayhan.aziz.nebi@gmail.com>
2026-05-11 01:40:31 +0300

    fix: Remove redundant cache clearing commands in docker entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -31,14 +31,12 @@ php artisan migrate --force --no-interaction
 # php artisan db:seed --class=SuperAdminSeeder --force --no-interaction
 
 echo "⚡ Clearing caches..."
-php artisan optimize:clear || true
+php artisan optimize:clear 
 
 echo "⚡ Optimizing..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan config:clear
-php artisan cache:clear || true
 
 php artisan storage:link || true
 echo "🌐 Starting server..."
